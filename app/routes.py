4b600f425380fcd0e309fb7a2c93df2933ece64a
Mason Edmison <mzbaran@uwm.edu>
2019-01-18 09:26:12 -0600

    db commit

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,27 @@
 from app import app
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import login_user, current_user, logout_user, login_required
+from app.forms import LoginForm
+from app.models import AdminUser
+
 
 @app.route('/')
 @app.route('/login')
-
 def login():
-	return 'hello world!'  
+    if current_user.is_authenticated:
+        pass
+    form = LoginForm()
+    if form.validate_on_submit:
+        user = AdminUser.query.filter_by(username=form.username.data).first()
+        if user is not None or not user.check_password(form.password.data):
+            flash('invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args('next')
+        if not next_page or url_for('index'):
+            pass
+        return(redirect(next_page))
+    return render_template('login.html', title=login, form=form)
+
+
+
